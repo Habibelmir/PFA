6446765a2164e63ead0699d8f7ecd75f8ca36a3a
Manal Maakoul <manalmaakoul22@gmail.com>
2024-03-15 03:25:44 +0000

    Class dbContext n'essisite installation des packages git add MonContext.cs   test

---
+++ Projet_pfa/Models/MonContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Projet_pfa.Models
+{
+    public class MonContext :dbContext
+    {
+        public DbSet<User> Users { get; set; }  
+        public DbSet<Match>Matches { get; set; }
+        public DbSet<Equipe> Equipes { get; set; }
+        public DbSet<Joueur> Joueurs { get; set; }
+        public DbSet<MatchEquipe> MatcheEquipes { get; set; }
+        public DbSet<Paiement> Paiements { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
