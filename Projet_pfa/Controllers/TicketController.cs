cce8cf1f2566194361801dd5e0550e93e25dbf81
Habib Elmir <habibelmir8@gmail.com>
2024-03-31 17:48:58 +0000

    ajout de la partie billet static

---
+++ Projet_pfa/Controllers/TicketController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projet_pfa.ViewModel;
+
+namespace Projet_pfa.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult MatchInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MatchInfo(MatchInformations model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("MatchInfo");
+            }
+            ViewBag.nbrtickets = model.NbrTicket;
+            return View("ticketInfo");
+        }
+        
+        public IActionResult ticketInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ticketInfo(TicketInformation model)
+        {
+            List<string> nomcomplets = model.NomComplets;
+            ViewBag.nomcompletsCount = nomcomplets.Count;
+            ViewBag.nomcomplets = nomcomplets;
+            return View();
+        }
+
+
+        public IActionResult Recapitulatif()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Projet_pfa/ViewModel/MatchInformations.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet_pfa.ViewModel
+{
+    public enum Categorie
+    {
+        CategorieA,
+        CategorieB, 
+        CategorieC
+    };
+   
+    public class MatchInformations
+    {
+        //possible que cette property soit une enumeration ! 
+        [Display(Name = "Catégorie de Siège")]
+        [Required(ErrorMessage ="Veuillez choisir une categorie")]
+        public Categorie CategorieSiege { get; set; }
+
+        [Display(Name ="Nombre de Ticket")]
+        [Required(ErrorMessage ="Veuillez choisir un nombre de tickets")]
+        public int NbrTicket{ get; set;}
+    }
+}
