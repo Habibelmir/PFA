a833184deceee578f19448026059b6168d0eccf3
Manal Maakoul <manalmaakoul22@gmail.com>
2024-03-31 02:00:43 +0000

    Modification MatchEquipe

--- Projet_pfa/Models/MatchEquipe.cs
+++ Projet_pfa/Models/MatchEquipe.cs
@@ -6,11 +6,8 @@ namespace Projet_pfa.Models
     {
         public int Id { get; set; }
         public Equipe Equipe { get; set; }
-        [ForeignKey("Equipe")]
         public int EquipeId { get; set; }
         public Match Match { get; set; }
-
-        [ForeignKey("Match")]
         public int MatchId { get; set; }
     }
 }
