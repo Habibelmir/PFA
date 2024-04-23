3c650c11a06a13ab28fb868291e30bf1b3651429
Habibelmir <habibelmir8@gmail.com>
2024-04-23 11:51:24 +0000

    ajout equipe apres modif

--- Projet_pfa/Models/Equipe.cs
+++ Projet_pfa/Models/Equipe.cs
@@ -2,18 +2,18 @@
 
 namespace Projet_pfa.Models
 {
-    [Table("Equipe")]
-    public class Equipe
-    {
-        public int Id { get; set; }
-        public string Nom { get; set; }
-        public string Drapeau {  get; set; }
-        
-        public IList<Match> MatchesEquipe1 { get; set; }
-        public IList<Match> MatchesEquipe2 { get; set; }
-        public IList<Joueur> Joueurs { get; set; }
-        public Groupe Groupe { get; set; }
-        public int GroupeId { get; set; }
-        
-    }
+	[Table("Equipe")]
+	public class Equipe
+	{
+		public int Id { get; set; }
+		public string Nom { get; set; }
+		public string Drapeau {  get; set; }
+		
+		public IList<Match> MatchesEquipe1 { get; set; }
+		public IList<Match> MatchesEquipe2 { get; set; }
+		public IList<Joueur> Joueurs { get; set; }
+		public Groupe Groupe { get; set; }
+		public int GroupeId { get; set; }
+		
+	}
 }
