4575d69b9901eebd2ce05d741135e2003d223c65
Habibelmir <habibelmir8@gmail.com>
2024-04-24 14:29:49 +0000

    partie ticket

---
+++ Projet_pfa/Mappers/MatchInformationMapper.cs
@@ -0,0 +1,23 @@
+﻿using Projet_pfa.Models;
+using Projet_pfa.ViewModel;
+
+namespace Projet_pfa.Mappers
+{
+    public class MatchInformationMapper
+    {
+        public static MatchInformations GetMatchInfoFromMatch(Match m)
+        {
+            string dateOnly = m.Date.ToString().Substring(0, 10);
+            MatchInformations matchinfo = new MatchInformations
+            {
+                equipe1 = m.Equipe1,
+                equipe2 = m.Equipe2,
+
+                date = dateOnly
+            };
+
+
+            return matchinfo;
+        }
+    }
+}
